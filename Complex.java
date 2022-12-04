bfacbd02d6082032c872e6b31be060a734ba6833
MahmutOzgurKizil <ozgurkizil@hotmail.com>
2022-12-04 11:11:47 +0300

    Create Complex.java
    Complex class

---
+++ Complex.java
@@ -0,0 +1,20 @@
+public class Complex{
+	private double num;
+	private double complex;
+	
+	public Complex(double a; double b){
+		num = a;
+		complex = b;
+	}
+	
+	public void setNum(double a){ num = a; }
+	public void setComplex(double a){ complex = a; }
+	public double getNum(){return num;}
+	public dobule getComplex(){return complex;}
+	
+	
+	
+	
+	
+	
+}
